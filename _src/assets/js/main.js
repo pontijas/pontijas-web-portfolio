9c3d8960504c651e00944ef013f029160564015b
cristinapontiijas <cristinapontijas@gmail.com>
2020-05-19 16:07:33 +0200

    add javascript event to navigation menu

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('holi mundi');
+
+const menuBtn = document.querySelector('.js-btn');
+const displayMenu = document.querySelector('.js-menu');
+
+function showMenu() {
+  console.log('me han clickado');
+  displayMenu.classList.toggle('menu-hidden');
+}
+
+menuBtn.addEventListener('click', showMenu);
