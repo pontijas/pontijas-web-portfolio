6740c76aedffacc5c8cf7c2825fcb45fb0dd8fa3
cristinapontiijas <cristinapontijas@gmail.com>
2020-05-19 16:23:27 +0200

    update javascript functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,10 +4,15 @@ console.log('holi mundi');
 
 const menuBtn = document.querySelector('.js-btn');
 const displayMenu = document.querySelector('.js-menu');
+const logoBtn = document.querySelector('.js-logo');
+const displayTitle = document.querySelector('.js-title');
 
 function showMenu() {
-  console.log('me han clickado');
   displayMenu.classList.toggle('menu-hidden');
+}
+function showTitle() {
+  displayTitle.classList.toggle('title-hidden');
 }
 
 menuBtn.addEventListener('click', showMenu);
+logoBtn.addEventListener('click', showTitle);
